113062ff2c3754afffac9cbc1f2ff77d1319a67c
Uriel García Rivas <44953654+Garz4@users.noreply.github.com>
2020-02-08 14:13:59 -0600

    Update AFN.java

--- AFN.java
+++ AFN.java
@@ -4,14 +4,91 @@ class AFN {
     HashSet<char> alfabeto;
     HashSet<Estado> edosAFN;
     
-    AFN(void){
-        this.edoIni = NULL;
-        this.alfabeto = new HashSet<char>();
-        this.alfabeto.clear();
-        this.edosAceptacion = new HashSet<Estado>();
+    //=====
+    
+    HashSet<char> getAlfabeto(){return this.alfabeto;}
+    HashSet<Estado> getEdosAFN(){return this.edosAFN;}
+    
+    AFN(){
+        edoIni = NULL;
+        alfabeto = new HashSet<char>();
+        alfabeto.clear();
+        edosAceptacion = new HashSet<Estado>();
+        edosAceptacion.clear();
+        edosAFN = new HashSet<Estado>();
+        edosAFN.clear();
+    }
+    
+    AFN(char s){
+        Estado ef;
+        edoIni = new Estado();
+        ef = new Estado();
+        ef.setAceptacion(true);
+        edosAceptacion = new HashSet<Estado>();
+        edosAceptacion.clear();
+        edosAceptacion.add(ef);
+        edosAFN = new HashSet<Estado>();
+        edosAFN.clear();
+        edosAFN.add(edoIni);
+        edosAFN.add(ef);
+        alfabeto = new HashSet<char>();
+        alfabeto.clear();
+        alfabeto.add(s);
+        edoIni.addTransicion(new Transicion(s, ef));
+    }
+    
+    AFN AFNBasico(char s){
+        Estado ef;
+        edoIni = new Estado();
+        ef = new Estado();
+        ef.setAceptacion(true);
+        edosAceptacion = new HashSet<Estado>();
+        edosAceptacion.clear();
+        edosAceptacion.add(ef);
+        edosAFN = new HashSet<Estado>();
+        edosAFN.clear();
+        edosAFN.add(edoIni);
+        edosAFN.add(ef);
+        alfabeto = new HashSet<char>();
+        alfabeto.clear();
+        alfabeto.add(s);
+        edoIni.addTransicion(new Transicion(s, ef));
+        return this;
+    }
+    
+    AFN unirAFN(AFN f){
+        Estado e1 = new Estado();
+        Estado e2 = new Estado();
+        
+        e1.addTransicion(new Transicion(Epsilon, this.edoIni));
+        e1.addTransicion(new Transicion(Epsilon, f.edoIni));
+        
+        for(Estado e : this.edosAceptacion){
+            e.addTransicion(new Transicion(Epsilon, e2));
+            e.setAceptacion(false);
+        }
+        
+        for(Estado e : f.edosAceptacion){
+            e.addTransicion(new Transicion(Epsilon, e2));
+            e.setAceptacion(false);
+        }
+        
+        e2.setAceptacion(true);
+        
+        this.alfabeto.addAll(f.getAlfabeto());
+        this.edosAFN.addAll(f.getEdosAFN());
+        this.edosAFN.add(e1);
+        this.edosAFN.add(e2);
         this.edosAceptacion.clear();
-        this.edosAFN = new HashSet<Estado>();
-        this.edosAFN.clear();
+        this.edosAceptacion.add(e2);
+        this.edoIni = e1;
+        f = null;
+        
+        return this;
+    }
+    
+    AFN concatenar(AFN f){ //Thompson
+        return this;
     }
 }
 
