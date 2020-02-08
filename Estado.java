e1590a5f5192ea0b090340ff2d42857dc4daa7cc
Uriel García Rivas <44953654+Garz4@users.noreply.github.com>
2020-02-07 22:27:34 -0600

    Update Estado.java

--- Estado.java
+++ Estado.java
@@ -1,3 +1,13 @@
 public class Estado {
+    private static int generalId;
+    private int id;
     
+    //=====
+    
+    public int getId(void){return this.id;}
+    
+    public Estado(void){
+        this.id = generalId;
+        generalId++;
+    }
 }
