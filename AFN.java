27074df1c72cc78aaffedf9f57353f92968f392c
Uriel García Rivas <44953654+Garz4@users.noreply.github.com>
2020-02-07 22:39:00 -0600

    Update AFN.java

--- AFN.java
+++ AFN.java
@@ -1,3 +1,32 @@
 class AFN {
 
 }
+
+/*
+Conjunto<Estado> Cerradura_e(Estado e){
+    Conjunto<Estado> R = new Conjunto<Estado>();
+    Pila<Estado> P = new Pila<Estado>();
+
+    P.push(e);
+
+    while(!P.vacia()){
+        e2 = P.pop();
+        if(R.contiene())
+            continue;
+        R.agregar(e2);
+        for each(Transicion t in e2.transiciones)
+            if(t.Simbolo == epsilon)
+                if(t.Estado no está en R)
+                    P.push(t.Estado);
+    }
+    return R;
+}
+
+Conjunto<Estado> Cerradura_e(Conjunto<Estado> c){
+    Conjunto<Estado> R = new Conjunto<Estado>();
+    R = {};
+    for each(Estado in c)
+        R.union(Cerradura_e(e));
+    return R;
+}
+*/
