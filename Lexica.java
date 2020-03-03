3826f6b2a6956ff5702fe94c93e13ac78a4cc65f
Uriel García Rivas <44953654+Garz4@users.noreply.github.com>
2020-03-03 11:11:02 -0600

    Update Lexica.java

--- Lexica.java
+++ Lexica.java
@@ -1,1 +1,27 @@
-
+class Lexica {
+    String lexema;
+    String total;
+    int currentIndex;
+    int token;
+    AFD automata;
+    
+    //=========================
+    
+    Lexica(){
+        this.total = "";
+        this.currentIndex = 0;
+    }
+    
+    Lexica(String toTotal, AFD toAutomata){
+        this.total = toTotal;
+        this.currentIndex = 0;
+        this.automata = toAutomata;
+    }
+    
+    public void compute(){
+        
+    }
+    
+    int getToken(){ return this.token; }
+    String getLexema(){ return this.lexema; }
+}
